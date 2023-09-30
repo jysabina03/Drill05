517583f4c9b2a02592fb90418fd626d5d9653199
jysabina03 <jysabina03@tukorea.ac.kr>
2023-09-30 23:24:35 +0900

    add make_load, make_hand
    hand, load(경로) 추가

--- line.py
+++ line.py
@@ -6,52 +6,61 @@ TUK_WIDTH,TUK_HEICHT=1280,1024
 open_canvas(TUK_WIDTH,TUK_HEICHT)
 character = load_image('animation_sheet.png')
 tuk_ground = load_image('TUK_GROUND.png')
-
-# def stop():
-#     turtle.bye()
-#
-# def draw_big_point(p):
-#     turtle.goto(p)
-#     turtle.color(0.8, 0.9, 0)
-#     turtle.dot(15)
-#     turtle.write('     '+str(p))
-#
-#
-# def draw_point(p):
-#     turtle.goto(p)
-#     turtle.dot(5, random.random(), random.random(), random.random())
-#
-#
-# def draw_line(p1, p2):
-#     # fill here
-#     draw_big_point(p1)
-#     draw_big_point(p2)
-#
-#     x1,y1=p1[0],p1[1]
-#     x2,y2=p2[0],p2[1]
-#
-#     for i in range(0,100+1,4):
-#         t=i/100
-#         x=(1-t)*x1+t*x2
-#         y=(1-t)*y1+t*y2
-#         draw_point((x,y))
-#     draw_point(p2)
+hand = load_image('hand_arrow.png')
 
 def make_hand():
-    pass
+    global handX,handY,speed
+    handX = random.randint(0,TUK_WIDTH)
+    handY = random.randint(0,TUK_HEICHT)
+    speed=0
+    global nowX, dir_see
 
-def make_load():
-    pass
+    if nowX < handX:
+        dir_see = 1
+    else:
+        dir_see = -1
 
-clear_canvas()
-tuk_ground.draw(TUK_WIDTH//2,TUK_HEICHT//2)
+def make_load(p1,p2):
+    x1,y1=p1[0],p1[1]
+    x2,y2=p2[0],p2[1]
 
-update_canvas()
-#handle_events()
-delay(0.1)
+    global speed
+    t=speed/100
 
-# fill here
-#draw_line((-100,-100),(300,150))
+    global nowX, nowY
+    x = (1-t) * x1 + t*x2
+    y = (1-t) * y1 + t*y2
+
+    nowX, nowY =x,y
+
+
+running = True
+charX,charY = TUK_WIDTH // 2,TUK_HEICHT//2
+handX,handY = charX,charY
+nowX,nowY=charX,charY
+dir_see = 1
+frame = 0
+speed = 0
+
+while running:
+    clear_canvas()
+    tuk_ground.draw(TUK_WIDTH//2,TUK_HEICHT//2)
+
+    if handX == nowX and handY == nowY:
+        make_hand()
+    make_load((nowX,nowY),(handX,handY))
+    speed+=4
+
+    hand.clip_draw(0, 0, 50, 52, handX, handY)
+
+    if dir_see == 1:
+        character.clip_draw(frame * 100, 0, 100, 100, nowX, nowY)
+    else:
+        character.clip_composite_draw(frame * 100, 0, 100, 100, 0,'h',nowX, nowY,100,100)
+
+    frame = (frame + 1) % 8
+    update_canvas()
+    delay(0.1)
 
 
 close_canvas()
